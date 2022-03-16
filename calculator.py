5c1344731d4b5a8f19cd60bc1941dd2fc8b66d8c
Laurel Mossor <laurel.mossor@gmail.com>
2022-03-16 16:20:45 -0700

    Changed tokens to float and add function

--- calculator.py
+++ calculator.py
@@ -8,23 +8,26 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 # loop through the list, index 0 = operand, 1 = num1, and 2 = num2
 
 def calculate():
-    input_string = input("What do you want to calculate?: \n> ")
+    while True:
+        input_string = input("What do you want to calculate?: \n> ")
 
-    if 'q' in input_string:
-        print('later!')
-        return
+        if 'q' in input_string:
+            print('later!')
+            break
 
-    tokens = input_string.split(' ')
-    print(tokens)
-    operand = tokens[0]
-    print(operand)
-    num1 = tokens[1]
-    print(num1)
-    num2 = tokens[2]
-    print(num2)
+        tokens = input_string.split(' ')
+        print(tokens)
+        operand = tokens[0]
+        print(operand)
+        num1 = float(tokens[1])
+        print(num1)
+        num2 = float(tokens[2])
+        print(num2)
 
-
-
+        if operand == "+":
+            print(add(num1, num2))
+    
+    
 
 
 calculate()
